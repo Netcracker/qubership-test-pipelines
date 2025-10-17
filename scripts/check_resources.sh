fade01f443e66048d65259f973a8ab2729dd02ca
KryukovaPolina <32718724+KryukovaPolina@users.noreply.github.com>
2025-10-17 15:37:22 +0300

    add check resources bash script

--- scripts/check_resources.sh
+++ scripts/check_resources.sh
@@ -1,11 +1,11 @@
 check_resources() {
+    echo 'Start checking resources'
     namespace="$1"
     deployments=$(kubectl get deployments -n "$namespace" -o jsonpath='{.items[*].metadata.name}')
     for deployment in $deployments; do
         echo "Deployment: $deployment"
         ready=$(kubectl get deployment "$deployment" -n "$namespace" -o jsonpath='{.status.readyReplicas}' 2>/dev/null)
         total=$(kubectl get deployment "$deployment" -n "$namespace" -o jsonpath='{.status.replicas}' 2>/dev/null)
-        echo "${ready:-0}/${total:-0} replicas are ready"
         if [[ "$ready" -ne "$total" ]] && [[ "$total" -gt 0 ]]; then
             echo "Deployment $deployment is not ready"
             return 1
@@ -17,7 +17,6 @@ check_resources() {
         echo "StatefulSet: $statefulset"
         ready=$(kubectl get statefulset "$statefulset" -n "$namespace" -o jsonpath='{.status.readyReplicas}' 2>/dev/null)
         total=$(kubectl get statefulset "$statefulset" -n "$namespace" -o jsonpath='{.status.replicas}' 2>/dev/null)
-        echo "${ready:-0}/${total:-0} replicas are ready"
         if [[ "$ready" -ne "$total" ]] && [[ "$total" -gt 0 ]]; then
             echo "StatefulSet $statefulset is not ready"
             return 1
@@ -29,7 +28,6 @@ check_resources() {
         echo "DaemonSet: $daemonset"
         ready=$(kubectl get daemonset "$daemonset" -n "$namespace" -o jsonpath='{.status.numberReady}' 2>/dev/null)
         desired=$(kubectl get daemonset "$daemonset" -n "$namespace" -o jsonpath='{.status.desiredNumberScheduled}' 2>/dev/null)
-        echo "${ready:-0}/${desired:-0} replicas are ready"
         if [[ "$ready" -ne "$desired" ]] && [[ "$desired" -gt 0 ]]; then
             echo "DaemonSet $daemonset is not ready"
             return 1
