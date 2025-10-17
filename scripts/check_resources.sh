328d81ff8fc8508da8f26da34722e324c35c6e74
KryukovaPolina <32718724+KryukovaPolina@users.noreply.github.com>
2025-10-17 16:48:27 +0300

    add check resources bash script

--- scripts/check_resources.sh
+++ scripts/check_resources.sh
@@ -3,34 +3,34 @@ check_resources() {
     namespace="$1"
     deployments=$(kubectl get deployments -n "$namespace" -o jsonpath='{.items[*].metadata.name}')
     for deployment in $deployments; do
-        echo "Deployment: $deployment"
         ready=$(kubectl get deployment "$deployment" -n "$namespace" -o jsonpath='{.status.readyReplicas}' 2>/dev/null)
         total=$(kubectl get deployment "$deployment" -n "$namespace" -o jsonpath='{.status.replicas}' 2>/dev/null)
-        if [[ "$ready" -ne "$total" ]] && [[ "$total" -gt 0 ]]; then
-            echo "Deployment $deployment is not ready"
-            return 1
+        if [[ "$ready" -eq "$total" ]] && [[ "$total" -gt 0 ]]; then
+           echo "Deployment $deployment is ready"
+        else
+           return 1
         fi
     done
 
     statefulsets=$(kubectl get statefulsets -n "$namespace" -o jsonpath='{.items[*].metadata.name}')
     for statefulset in $statefulsets; do
-        echo "StatefulSet: $statefulset"
         ready=$(kubectl get statefulset "$statefulset" -n "$namespace" -o jsonpath='{.status.readyReplicas}' 2>/dev/null)
         total=$(kubectl get statefulset "$statefulset" -n "$namespace" -o jsonpath='{.status.replicas}' 2>/dev/null)
-        if [[ "$ready" -ne "$total" ]] && [[ "$total" -gt 0 ]]; then
-            echo "StatefulSet $statefulset is not ready"
-            return 1
+        if [[ "$ready" -eq "$total" ]] && [[ "$total" -gt 0 ]]; then
+            echo "StatefulSet $statefulset is ready"
+        else
+           return 1
         fi
     done
 
     daemonsets=$(kubectl get daemonsets -n "$namespace" -o jsonpath='{.items[*].metadata.name}')
     for daemonset in $daemonsets; do
-        echo "DaemonSet: $daemonset"
         ready=$(kubectl get daemonset "$daemonset" -n "$namespace" -o jsonpath='{.status.numberReady}' 2>/dev/null)
         desired=$(kubectl get daemonset "$daemonset" -n "$namespace" -o jsonpath='{.status.desiredNumberScheduled}' 2>/dev/null)
-        if [[ "$ready" -ne "$desired" ]] && [[ "$desired" -gt 0 ]]; then
-            echo "DaemonSet $daemonset is not ready"
-            return 1
+        if [[ "$ready" -eq "$desired" ]] && [[ "$desired" -gt 0 ]]; then
+            echo "DaemonSet $daemonset is ready"
+        else
+           return 1
         fi
     done
 
