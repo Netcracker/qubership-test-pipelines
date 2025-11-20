3859d6f9681032cb61ff70e571191e420039eba3
KryukovaPolina <polina.kriukova.qubership@gmail.com>
2025-11-20 11:32:51 +0300

    feat: Add PGskipper smoke pipeline (#84)
    * feat: add pgskipper
    
    * feat: update verify action
    
    * fix: reconcile
    
    * refactor: update repository_name
    
    * feat: add templates
    
    * fix: update path to config
    
    * feat: update Check service is ready
    
    * refactor: rename timeout variables
    
    * feat: change charts-values-update-action version
    
    * feat: add check crd bash script, rename folder
    
    * feat: add check resources bash script
    
    * refactor: change actions
    
    * refactor: update checks
    
    * feat: add check_tests script
    
    * refactor: update cr check
    
    * refactor: update check resources
    
    * feat: update pg skipper pipeline
    
    * feat: add pgskipper pipeline
    
    * feat: update pgskipper pipeline
    
    * refactor: verify installation action
    
    * feat: add upgrade
    
    * fix: skip updating versions for release
    
    * fix: update templates
    
    * feat: update pgskipper pipeline
    
    * fix: update template for pg
    
    * fix: update conditions
    
    * fix: update check cr
    
    * feat: add Free Disk Space step
    
    * refactor: update pgskipper actions
    
    * fix: update timeout before services check
    
    * fix: return python folder
    
    ---------
    
    Co-authored-by: Roman Barmin <roman.barmin.qubership@gmail.com>

---
+++ scripts/check_tests.sh
@@ -0,0 +1,51 @@
+check_tests() {
+    local namespace="$1"
+    local file_name="artifacts/${namespace}_tests.txt"
+    local IFS=" "
+    local -a pods
+    IFS=" " read -ra pods <<< "$(kubectl get pods --no-headers -o custom-columns=":metadata.name" -n "$namespace" | tr "\n" " ")"
+    local test_pod=""
+
+    for pod in "${pods[@]}"; do
+      if [[ "$pod" == *"tests"* ]]; then
+        test_pod="$pod"
+        break
+      fi
+    done
+
+    if ! [[ "$test_pod" ]]; then
+      echo "::error:: ❌ There is no test pod"
+      exit 2
+    fi
+
+    echo "Test pod found: $test_pod"
+
+    status=$(kubectl get pod "$test_pod" -n "$namespace" -o jsonpath="{.status.phase}")
+    echo "Pod status: $status"
+
+    logs=$(kubectl logs "$test_pod" -n "$namespace")
+    if [[ "$logs" =~ Report.*html ]]; then
+      echo "$logs" > "$file_name"
+      echo "📄 TEST LOGS:"
+      echo "$logs"
+
+      if ! kubectl cp "$test_pod":/opt/robot/output artifacts/robot-results -n "$namespace"; then
+        echo "::warning:: ⚠️ Failed to copy robot results"
+      else
+        echo "Robot results copied successfully"
+      fi
+
+      if [[ "$logs" == *"| FAIL |"* ]]; then
+        #Tests failed
+        exit 2
+      else
+        #Tests passed successfully
+        exit 0
+      fi
+    else
+       #Tests are still running
+       exit 1
+    fi
+}
+
+check_tests "$@"
