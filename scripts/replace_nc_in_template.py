ca4ae85096302b98706400d9a8d936b9e854b2cf
Gravelord <vigilante2084@gmail.com>
2026-02-26 07:30:05 +0300

    feat: revert nc replace

---
+++ scripts/replace_nc_in_template.py
@@ -0,0 +1,24 @@
+import os
+import yaml
+import argparse
+
+
+def main(args_):
+    path_to_file = args_.path_to_file
+    with open(path_to_file) as file:
+        try:
+            template = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    template['metadata']['namespace'] = args_.nc
+    with open(path_to_file, 'w') as file:
+        file.write(yaml.dump(template, default_flow_style=False))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script to replace namespace in templates of resources')
+    parser.add_argument('--path_to_file', type=str,
+                        help='path to yaml file with template')
+    parser.add_argument('--nc', type=str,
+                        help='new namespace')
+    args = parser.parse_args()
+    main(args)
