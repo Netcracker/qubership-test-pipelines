9b134cf81ad85a404af5b3e6200cb71071babe19
KryukovaPolina <32718724+KryukovaPolina@users.noreply.github.com>
2026-06-18 15:30:33 +0300

    refactor: add test_pod_prefix variable

--- scripts/check_tests.sh
+++ scripts/check_tests.sh
@@ -1,19 +1,20 @@
 check_tests() {
     local namespace="$1"
+    local pod_prefix="$2"
     local IFS=" "
     local -a pods
     IFS=" " read -ra pods <<< "$(kubectl get pods --no-headers -o custom-columns=":metadata.name" -n "$namespace" | tr "\n" " ")"
     local test_pod=""
 
     for pod in "${pods[@]}"; do
-      if [[ "$pod" == *"tests"* ]]; then
+      if [[ "$pod" == *"$pod_prefix"* ]]; then
         test_pod="$pod"
         break
       fi
     done
 
     if ! [[ "$test_pod" ]]; then
-      echo "::warning:: ℹ️ check_tests set to 'true', but there is no test pod"
+      echo "::warning:: ℹ️ No test pod found in namespace $namespace"
       exit 0
     fi
 
